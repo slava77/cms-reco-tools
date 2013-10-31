a29011fe3c089e64dbb969b140683bdf2b82ea13
Slava Krutelyov <slava77@cern.ch>
2013-10-31 13:30:06 +0100

    compare new/missing branches with a pattern

---
+++ diffBranches.sh
@@ -0,0 +1,7 @@
+or=$1
+nw=$2
+pPat="RECO"
+[ "x$3" != "x" ] && pPat=$3
+grep _"[.]*${pPat}" $or | while read -r ob ou uc; do grep -w $ob $nw >& /dev/null || echo gone: $ob $uc; done
+grep _"[.]*${pPat}" $nw | while read -r ob ou uc; do grep -w $ob $or >& /dev/null || echo  new: $ob $uc; done
+
