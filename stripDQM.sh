3e9b5b84a552ce239d43c164731ce7ee0ab87c19
Slava Krutelyov <slava77@cern.ch>
2015-01-13 01:35:54 +0100

    no need to pass run number, it's unique

--- stripDQM.sh
+++ stripDQM.sh
@@ -1,8 +1,5 @@
 f=$1
-r=$3
-if [ "x${r}" == "x" ]; then
-    r=1
-fi
+
 pattRemove="AAABBBCCCDDD"
 #"AlcaBeamMonitor\|Btag/Run\|1/HLT/Run\|ry/DiMuonH\|mmary/Isolation\|mmary/MuonKinVsEta\|RecoMuon_MuonAssoc_Sta\|1/Pixel/Run\|1/RPC/Run\|1/SiStrip/Run\|cutsRecoThird_\|cutsRecoSecond_\|cutsRecoZero_\|cutsRecoSixth_\|cutsRecoFifth_\|cutsRecoFirst_\|cutsRecoFourth_\|cutsReco_"
 [ "x${2}" != "x" ] && pattRemove="${pattRemove}\|$2"
@@ -12,7 +9,7 @@ if [ "${fExt}" != "ps" ]; then
     echo Need a ps extension
     exit 1
 fi
-range=`grep "\%Page\|Run ${r}/" $f  | sed -e 's?[^(]*\((/DQM[^)]*)\) [^$]*?\1?g;s/$^(//g' |\
+range=`grep "\%Page\|Run [0-9]\{1,6\}/" $f  | sed -e 's?[^(]*\((/DQM[^)]*)\) [^$]*?\1?g;s/$^(//g' |\
  awk '/^%%Page/{pg=$3}/^\(\/DQM/{nm=$0; print pg" "nm}' |\
  grep -v "${pattRemove}" |\
  awk 'BEGIN{lx=0;n=0}{if(n==0){f=$1;lx=f}nx=$1;n++; if(nx-lx>1){printf f"-"lx",";f=nx;} lx=nx}END{print f"-"lx}'`
