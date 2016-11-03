73f7f4b8f6b05a9fc3518ecdf640c772339900ef
Slava Krutelyov <slava77@cern.ch>
2016-11-03 18:16:08 +0100

    print 4MB+ changes in the current memory

--- monitorProcStat.sh
+++ monitorProcStat.sh
@@ -3,4 +3,4 @@ jid=$1
 while [ -f /proc/$jid/statm ]; do 
     cat /proc/$jid/statm
     sleep 0.1 
-done | awk 'BEGIN{max=0;n=0}{if($2>max){max=$2; tstr=strftime(); print n" "max*4" "tstr;};n++; if(n%100==0)print n" "max*4" "tstr" Now "$2*4" "strftime()}END{print max*4}'
+done | awk 'BEGIN{max=0;n=0}{if($2>max){max=$2; tstr=strftime(); print n" "max*4" "tstr;};n++; if(n%100==0||$2-last>1000||$2-last<-1000)print n" "max*4" "tstr" Now "$2*4" "strftime(); last=$2;}END{print max*4}'
