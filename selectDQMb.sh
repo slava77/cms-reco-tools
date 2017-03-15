95f8a4a04e8992527536e18aeb326ad5c442a747
Slava Krutelyov <slava77@cern.ch>
2017-03-15 13:00:53 +0100

    full page patter text search select/strip scripts

---
+++ selectDQMb.sh
@@ -0,0 +1,18 @@
+f=$1
+
+pattSelect="ZZZZZZZZ"
+[ "x${2}" != "x" ] && pattSelect="${pattSelect}\|$2"
+fExt=`echo $f | tr '.' '\n' | tail -1`
+fB=`echo ${f} | sed -e "s?.${fExt}??g"`
+if [ "${fExt}" != "ps" ]; then
+    echo Need a ps extension
+    exit 1
+fi
+range=`grep "\%Page\|Run [0-9]\{1,6\}/\|${pattSelect}" $f  | sed -e 's?[^(]*\((/DQM[^)]*)\) [^$]*?\1?g;s/$^(//g' |\
+ awk '/^%%Page/{pg=$3}{nm=$0; print pg" "nm}' |\
+ grep  "${pattSelect}" |\
+ awk 'BEGIN{lx=0;n=0}{if(n==0){f=$1;lx=f}nx=$1;n++; if(nx-lx>1){printf f"-"lx",";f=nx;} lx=nx}END{print f"-"lx}'`
+if [ "x${range}" != "x" ]; then
+    echo will strip ${range} from ${f}
+    psselect -p${range} $f ${fB}_strip.${fExt} 2> /dev/null
+fi
