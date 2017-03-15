fd0644e330811f246ffbb0f852b514213ab2b805
Slava Krutelyov <slava77@cern.ch>
2017-03-15 12:52:22 +0100

    pick 13 TeV versions for 2017 workflows

--- extendedTests2017.sh
+++ extendedTests2017.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "started QCD wf 10043.0"
-runTheMatrix.py -l 10043.0 -w upgrade --useInput all --command="-n 600 --customise Validation/Performance/TimeMemoryInfo.py" >& run10043.0_600.log &
+echo "started QCD wf 10071.0"
+runTheMatrix.py -l 10071.0 -w upgrade --useInput all --command="-n 600 --customise Validation/Performance/TimeMemoryInfo.py" >& run10071.0_600.log &
 echo "started TTbar PU 25ns  wf 10224.0"
 runTheMatrix.py -l 10224.0 -w upgrade --useInput all --command=" -n 100 --customise Validation/Performance/TimeMemoryInfo.py" >& run10224.0_100.log &
-echo "started QCD 3TeV wf 10030.0"
-runTheMatrix.py -l 10030.0 -w upgrade --useInput all --command="-n 400 --customise Validation/Performance/TimeMemoryInfo.py" >& run10030.0_400.log &
+echo "started QCD 3TeV wf 10059.0"
+runTheMatrix.py -l 10059.0 -w upgrade --useInput all --command="-n 400 --customise Validation/Performance/TimeMemoryInfo.py" >& run10059.0_400.log &
 if [ "x${1}" == "xALL" ]; then 
     echo "started 2 e/2 gamma/2 mu guns: 10002.0,10003.0,10004.0,10005.0,10007.0,10009.0"
     runTheMatrix.py -l 10002.0,10003.0,10004.0,10005.0,10007.0,10009.0 -w upgrade --useInput all --command="-n 1000" -j 12 >& runGuns.log &
